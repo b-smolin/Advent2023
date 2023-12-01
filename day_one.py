039c05455e3173b944382cc2ae0c0128f33edede
Ben Smolin <bsmolin11@gmail.com>
2023-11-30 23:38:51 -0500

    initial commit

---
+++ day_one.py
@@ -0,0 +1,18 @@
+def parseInput():
+    f = open("inputs/day1.txt", "r")
+
+    f.close()
+
+def partOne():
+    print("pass part 1")
+
+def partTwo():
+    print("pass part 2")
+
+def main():
+    input = parseInput()
+    partOne()
+    partTwo()
+
+if __name__ == "__main__":
+    main()
